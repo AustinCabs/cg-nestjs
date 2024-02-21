a5e67c834789dbced524f8a31d2c6777e5f8664a
AustinCabs <austinbrien.cabanada@gmail.com>
2024-02-21 13:10:22 +0800

    added event resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
